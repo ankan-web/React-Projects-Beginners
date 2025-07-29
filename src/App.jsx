946380158fe792655d82cfe3f2b9635c488aec27
ankan mondal <ankan9353@gmail.com>
2025-07-29 22:24:21 +0530

    -Counter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import Todo from './component/Todo'
+import Counter from './component/Counter'
+
+const App = () => {
+  return (
+    <div>
+      <Counter/>
+      {/* <Todo/> */}
+    </div>
+  )
+}
+
+export default App
+
